3526aeec14735e1062525862953cf688f6b4fbda
Duong Thai Tran <duongthaitran68@gmail.com>
2025-08-04 20:35:39 +0700

    refactoer code: create routes, app/controllers folders

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
 
 //midware
@@ -28,23 +30,10 @@ app.engine(
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "resources", "views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+// Home, search, contact
 
-app.get("/news", (req, res) => {
-  //console.log(req.query.q);
-  res.render("news");
-});
-
-app.get("/search", (req, res) => {
-  res.render("search");
-});
-
-app.post("/search", (req, res) => {
-  console.log(req.body.q);
-  res.send("HEHE");
-});
+// Routes init
+route(app);
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
