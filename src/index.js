572ff3168fbe012113f10f5f9ec734f39d0d1bcd
Duong Thai Tran <duongthaitran68@gmail.com>
2025-08-05 17:15:41 +0700

    Course detail page

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const app = express();
 const port = 3000;
 
 const route = require('./routes');
+const db = require('./config/db');
+
+// Connect to db
+db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -36,5 +40,5 @@ app.set('views', path.join(__dirname, 'resources', 'views'));
 route(app);
 
 app.listen(port, () =>
-    console.log(`Example app listening at http://localhost:${port}`),
+    console.log(`App listening at http://localhost:${port}`),
 );
