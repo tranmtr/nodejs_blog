85a62599d86647ed625fe880884e21e32b597095
Duong Thai Tran <duongthaitran68@gmail.com>
2025-08-08 17:18:53 +0700

    Update course

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const methodOverride = require('method-override');
 const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
@@ -21,6 +22,7 @@ app.use(
 ); // form
 app.use(express.json()); // XMLHttpRequest,  ...
 
+app.use(methodOverride('_method'));
 // HTTP logger
 //app.use(morgan("combined"));
 
@@ -29,6 +31,9 @@ app.engine(
     '.hbs',
     handlebars.engine({
         extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        },
     }),
 );
 app.set('view engine', '.hbs');
